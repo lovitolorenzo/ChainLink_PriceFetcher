ae30f7107470dc61e4acf871884031b5fc36ad00
Lorenzo <lovitolorenzo@yahoo.it>
2022-12-23 12:15:01 +0100

    creates hardhat environment

---
+++ hardhat.config.ts
@@ -0,0 +1,41 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const { API_URL, RESERVE3_PRIVATE_KEY } = process.env;
+
+module.exports = {
+	solidity: "0.8.17",
+	defaultNetwork: "goerli",
+	networks: {
+		hardhat: {},
+		goerli: {
+			accounts: [RESERVE3_PRIVATE_KEY ? RESERVE3_PRIVATE_KEY : ""], //deployer
+			url: API_URL,
+			//blockGasLimit: 100000000000000,
+		},
+	},
+};
+
+// module.exports = {
+// 	solidity: {
+// 		version: "0.8.17",
+// 		settings: {
+// 			optimizer: {
+// 				enabled: true,
+// 				runs: 200,
+// 			},
+// 		},
+// 	},
+// 	paths: {
+// 		sources: "./contracts",
+// 	},
+// 	defaultNetwork: "matic",
+// 	networks: {
+// 		hardhat: {},
+// 		matic: {
+// 			url: API_URL,
+// 			accounts: [RESERVE3_PRIVATE_KEY ? RESERVE3_PRIVATE_KEY : ""], //deployer
+// 		},
+// 	},
+// };
